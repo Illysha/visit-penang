73bcd013b5b4b7af13ea729b73b3d694fcf5de77
Illysha <syamiela@student.usm.my>
2025-01-07 00:07:17 +0800

    pp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import App from './App';
 import './App.css';
 
-ReactDOM.render(
+// Use the createRoot API for React 18+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
+
 
 
