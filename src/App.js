d139860b45e562478fac6793fe03262530245855
Illysha <syamiela@student.usm.my>
2025-01-06 21:01:52 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import Router and Route components
 import TouristSpots from './TouristSpots';
 import FoodBeverages from './FoodBeverages';
 import Hotels from './Hotels';
 import CultureHeritage from './CultureHeritage';
 import AboutUs from './AboutUs'; // Import the AboutUs component
-import TouristSpotDescription from './TouristSpotDescription'; // Import the TouristSpotDescription component
 import './App.css';
 
 function App() {
     return (
-        <Router>  {/* Wrap your application with Router */}
-            <div className="App">
-                <header className="App-header">
-                    <h1>Visit Penang</h1>
-                    <p>Explore the best of Penang: tourist spots, food, hotels, culture and heritage!</p>
-                </header>
-                <Routes> {/* Use Switch to ensure only one route is matched */}
-                    <Route exact path="/" component={TouristSpots} /> {/* Main tourist spots page */}
-                    <Route path="/tourist-spot/:id" component={TouristSpotDescription} /> {/* Dynamic route for tourist spot descriptions */}
-                    <Route path="/food-beverages" component={FoodBeverages} /> {/* Food and beverages page */}
-                    <Route path="/hotels" component={Hotels} /> {/* Hotels page */}
-                    <Route path="/culture-heritage" component={CultureHeritage} /> {/* Culture and heritage page */}
-                    <Route path="/about-us" component={AboutUs} /> {/* About Us page */}
-                </Routes>
-            </div>
-        </Router>
+        <div className="App">
+            <header className="App-header">
+                <h1>Visit Penang</h1>
+                <p>Explore the best of Penang: tourist spots, food, hotels, culture and heritage!</p>
+            </header>
+            <TouristSpots />
+            <FoodBeverages />
+            <Hotels />
+            <CultureHeritage />
+            <AboutUs /> {/* Add the About Us section here */}
+        </div>
     );
 }
 
--- src/TouristSpots.js
+++ src/TouristSpots.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component for routing
 
 function TouristSpots() {
     return (
@@ -8,18 +7,8 @@ function TouristSpots() {
             <p>Explore the most popular and scenic tourist destinations in Penang.</p>
             {/* Add images for this category */}
             <div className="tourist-spots-images">
-                <Link to="/tourist-spot/1"> {/* Linking to the description page */}
-                    <img
-                        src="https://i.pinimg.com/236x/89/69/b4/8969b4030e55d439a8ea89b4ca76f12a.jpg"
-                        alt="Tourist Spot 1"
-                    />
-                </Link>
-                <Link to="/tourist-spot/2"> {/* Linking to another description page */}
-                    <img
-                        src="https://i.pinimg.com/236x/6d/be/6f/6dbe6f2bf57010a7020f7d24b50ac154.jpg"
-                        alt="Tourist Spot 2"
-                    />
-                </Link>
+                <img src="https://i.pinimg.com/236x/89/69/b4/8969b4030e55d439a8ea89b4ca76f12a.jpg" alt="Tourist Spot 1"/>
+                <img src="https://i.pinimg.com/236x/6d/be/6f/6dbe6f2bf57010a7020f7d24b50ac154.jpg" alt="Tourist Spot 2"/>
             </div>
             {/* Embed YouTube video */}
             <div className="tourist-spots-video">
