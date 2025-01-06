ad2ec89e6ab8be7c4387af528b130b584c671fce
Illysha <syamiela@student.usm.my>
2025-01-07 00:32:53 +0800

    kk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
+ // Use Routes instead of Switch
 import TouristSpots from './TouristSpots';
 import FoodBeverages from './FoodBeverages';
 import Hotels from './Hotels';
 import CultureHeritage from './CultureHeritage';
 import AboutUs from './AboutUs';
-
 import './App.css'; // Import your CSS styles
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <header className="App-header">
-                    <h1>Visit Penang</h1>
-                    <p>Explore the best of Penang: tourist spots, food, hotels, culture and heritage!</p>
-                </header>
-
-                {/* Add navigation for different sections */}
+            {/* Add navigation for different sections */}
                 <Routes>  {/* Use Routes here instead of Switch */}
                     <Route path="/" element={<div className="home"><h2>Home Page</h2><p>Welcome to the best places to visit in Penang!</p></div>} />
                     <Route path="/tourist-spots" element={<TouristSpots />} />
@@ -27,7 +20,6 @@ function App() {
                     <Route path="/culture-heritage" element={<CultureHeritage />} />
                     <Route path="/about-us" element={<AboutUs />} />
                 </Routes>
-            </div>
         </Router>
     );
 }
