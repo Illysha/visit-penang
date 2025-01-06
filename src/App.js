fdb972a13ca38db2fe3feecbd4b933d56a5f23ce
Illysha <syamiela@student.usm.my>
2025-01-06 23:00:15 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 
-    import React from 'react';
-import TouristSpots from './TouristSpots';
-import FoodBeverages from './FoodBeverages';
-import Hotels from './Hotels';
-import CultureHeritage from './CultureHeritage';
-import AboutUs from './AboutUs'; // Import the AboutUs component
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TouristSpots from './components/TouristSpots';
+import FoodBeverages from './components/FoodBeverages';
+import Hotels from './components/Hotels';
+import CultureHeritage from './components/CultureHeritage';
+import AboutUs from './components/AboutUs';
+
+import './App.css'; // Import your CSS styles
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Visit Penang</h1>
-                <p>Explore the best of Penang: tourist spots, food, hotels, culture and heritage!</p>
-            </header>
-            <TouristSpots />
-            <FoodBeverages />
-            <Hotels />
-            <CultureHeritage />
-            <AboutUs /> {/* Add the About Us section here */}
-        </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Visit Penang</h1>
+                    <p>Explore the best of Penang: tourist spots, food, hotels, culture and heritage!</p>
+                </header>
+
+                {/* Add navigation for different sections */}
+                <Switch>
+                    <Route path="/" exact>
+                        <div className="home">
+                            <h2>Home Page</h2>
+                            <p>Welcome to the best places to visit in Penang!</p>
+                        </div>
+                    </Route>
+
+                    <Route path="/tourist-spots">
+                        <TouristSpots />
+                    </Route>
+
+                    <Route path="/food-beverages">
+                        <FoodBeverages />
+                    </Route>
+
+                    <Route path="/hotels">
+                        <Hotels />
+                    </Route>
+
+                    <Route path="/culture-heritage">
+                        <CultureHeritage />
+                    </Route>
+
+                    <Route path="/about-us">
+                        <AboutUs />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
--- src/TouristSpots.js
+++ src/TouristSpots.js
@@ -1,28 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function TouristSpots() {
-    // useEffect hook to run the logic when the component is mounted
-    useEffect(() => {
-        // Get the 'Back to Home' link by its ID
-        const backToHomeLink = document.getElementById('back-to-home');
+    const history = useHistory();
 
-        // Add event listener to handle click event
-        const handleBackToHome = (event) => {
-            event.preventDefault(); // Prevent the default action (which would refresh the page)
-            window.location.href = 'index.html'; // Navigate to the home page (index.html)
-        };
-
-        if (backToHomeLink) {
-            backToHomeLink.addEventListener('click', handleBackToHome);
-        }
-
-        // Cleanup event listener when the component is unmounted
-        return () => {
-            if (backToHomeLink) {
-                backToHomeLink.removeEventListener('click', handleBackToHome);
-            }
-        };
-    }, []); // Empty dependency array ensures this runs only once when the component is mounted
+    const handleBackToHome = () => {
+        history.push('/'); // Navigate to the home page ("/")
+    };
 
     return (
         <div className="tourist-spots">
@@ -30,8 +14,10 @@ function TouristSpots() {
             <p>Explore the most popular and scenic tourist destinations in Penang.</p>
             {/* Add images for this category */}
             <div className="tourist-spots-images">
-                <img src="https://i.pinimg.com/236x/89/69/b4/8969b4030e55d439a8ea89b4ca76f12a.jpg" alt="Tourist Spot 1" />
-                <img src="https://i.pinimg.com/236x/6d/be/6f/6dbe6f2bf57010a7020f7d24b50ac154.jpg" alt="Tourist Spot 2" />
+                <img src="https://i.pinimg.com/236x/89/69/b4/8969b4030e55d439a8ea89b4ca76f12a.jpg"
+                     alt="Tourist Spot 1"/>
+                <img src="https://i.pinimg.com/236x/6d/be/6f/6dbe6f2bf57010a7020f7d24b50ac154.jpg"
+                     alt="Tourist Spot 2"/>
             </div>
             {/* Embed YouTube video */}
             <div className="tourist-spots-video">
@@ -45,10 +31,10 @@ function TouristSpots() {
                     title="Tourist spots Video"
                 ></iframe>
             </div>
-            {/* Back to Home link */}
-            <a href="#" id="back-to-home" className="link">Back to Home</a>
+            <button onClick={handleBackToHome} className="link">Back to Home</button>
         </div>
     );
 }
+
 
 export default TouristSpots;
