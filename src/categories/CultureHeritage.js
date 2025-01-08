bc62655cd54715f4b3e4dd5f97b94195fae55c3b
Illysha <syamiela@student.usm.my>
2025-01-08 13:25:34 +0800

    k

--- src/categories/CultureHeritage.js
+++ src/categories/CultureHeritage.js
@@ -10,17 +10,17 @@ export const CultureHeritage = () => {
             </p>
 
             {/* Image with Title */}
-            <figure>
+            <figure style={{textAlign: 'center',  marginBottom: '30px'}}>
                 <img
                     src="https://i.pinimg.com/236x/3b/94/54/3b9454bc687faa9c5bb61e228e8de3b5.jpg"
                     alt="Culture and Heritage"
-                    style={{ width: '100%', height: 'auto' }}
+                    style={{width: '560', height: '315'}}
                 />
                 <figcaption>Exploring the cultural heritage of George Town</figcaption>
             </figure>
 
             {/* YouTube Video */}
-            <div>
+            <div style={{textAlign: 'center'}}>
                 <h2>Watch: Cultural Insights of Penang</h2>
                 <iframe
                     width="560"
