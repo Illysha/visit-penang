2a70e33341a8811899969d4aa93b15807b156bb9
Illysha <syamiela@student.usm.my>
2025-01-09 00:41:13 +0800

    k

--- src/categories/TouristSpots.js
+++ src/categories/TouristSpots.js
@@ -18,7 +18,7 @@ export const TouristSpots = () => {
                 <iframe
                     width="100%"
                     height="315"
-                    src="https://youtu.be/gQ7bxektkU8?si=HDFxhQeyzBqxNmBT"
+                    src="https://www.youtube.com/embed/6UKN1YpVweg"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
