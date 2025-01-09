7a02eb4e6ddb9e4c128ecf6139d25f8feb2765a5
Illysha <syamiela@student.usm.my>
2025-01-10 00:57:17 +0800

    l

--- src/categories/AboutUs.js
+++ src/categories/AboutUs.js
@@ -35,37 +35,68 @@ export const AboutUs = () => {
             </div>
             <div className="news-row">
                 <div className="news-container">
-                    <h2>News 1</h2>
+                    <h2>NewStaritTimes</h2>
                     <ul className="news-list">
                         <li>
-                            <a href="https://example.com/news1" target="_blank" rel="noopener noreferrer">
-                                Exciting News Headline 1
+                            <a href="https://www.nst.com.my/news/nation/2025/01/1158680/lrt-project-set-alter-penangs-public-transportation-scene" target="_blank" rel="noopener noreferrer">
+                                LRT project set to alter Penang's public transportation scene
                             </a>
                         </li>
                     </ul>
                 </div>
                 <div className="news-container">
-                    <h2>News 2</h2>
+                    <h2>TodaysOnline</h2>
                     <ul className="news-list">
                         <li>
-                            <a href="https://example.com/news2" target="_blank" rel="noopener noreferrer">
-                                Exciting News Headline 2
+                            <a href="https://www.todayonline.com/8days/heritage-laksa-stall-penang-opens-pop-shop-spore-crispy-tempura-fish-laksa-worth-trying-2160926" target="_blank" rel="noopener noreferrer">
+                                Heritage laksa stall from Penang opens pop-up shop in S’pore, crispy tempura fish laksa worth trying
                             </a>
                         </li>
                     </ul>
                 </div>
                 <div className="news-container">
-                    <h2>News 3</h2>
+                    <h2>MalayMail</h2>
                     <ul className="news-list">
                         <li>
-                            <a href="https://example.com/news3" target="_blank" rel="noopener noreferrer">
-                                Exciting News Headline 3
+                            <a href="https://www.malaymail.com/news/malaysia/2021/09/15/penang-set-to-receive-second-unesco-recognition-with-designation-of-penang/2005784" target="_blank" rel="noopener noreferrer">
+                                Penang set to receive second Unesco recognition with designation of Penang Hill as a biosphere reserve
                             </a>
                         </li>
                     </ul>
                 </div>
             </div>
-
+            <div className="news-row">
+                <div className="news-container">
+                    <h2>Bernama</h2>
+                    <ul className="news-list">
+                        <li>
+                            <a href="https://www.bernama.com/en/news.php?id=2371042" target="_blank" rel="noopener noreferrer">
+                                Penang Second Bridge Will Be Temporarily Close
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="news-container">
+                    <h2>Today Online</h2>
+                    <ul className="news-list">
+                        <li>
+                            <a href="https://www.todayonline.com/world/chiak-pa-boey-penang-seeks-register-hokkien-dialect-state-heritage-2205196" target="_blank" rel="noopener noreferrer">
+                                ‘Chiak pa boey?' Penang seeks to register Hokkien dialect as state heritage
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="news-container">
+                    <h2>TheStraitsTime</h2>
+                    <ul className="news-list">
+                        <li>
+                            <a href="https://www.straitstimes.com/asia/se-asia/four-iconic-penang-murals-restored-by-lithuanian-artist-ernest-zacharevic-12-years-later" target="_blank" rel="noopener noreferrer">
+                                Four iconic Penang murals restored by Lithuanian artist Ernest Zacharevic after 12 years
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
 
